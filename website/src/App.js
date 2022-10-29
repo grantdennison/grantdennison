ad9648ac30b565cc92d43881c6d5ca1631da1745
Grant Dennison <grantdennison@yahoo.co.uk>
2022-10-29 03:25:05 +0100

    homepage

---
+++ website/src/App.js
@@ -0,0 +1,21 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Grant Dennison</h1>
+        <a
+          className="App-link"
+          href="https://grant-dennison.co.uk"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Website
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
